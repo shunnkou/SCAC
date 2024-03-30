28eb08a346d6e685e45e02c469dcf1e78fe1e34b
Jace Huang <jacehuang8@proton.me>
2024-03-30 17:50:07 -0400

    Open link in new tab
    Only open links in body text in new tab, not navigation links

--- docs/assets/js/open_links_in_new_tab.js
+++ docs/assets/js/open_links_in_new_tab.js
@@ -1,7 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var links = document.querySelectorAll('a');
-    links.forEach(function(link) {
+    // Select all links within the body text
+    var bodyLinks = document.querySelectorAll('.markdown a');
+    
+    // Loop through each body link and add the target attribute
+    bodyLinks.forEach(function(link) {
         link.setAttribute('target', '_blank');
-        link.setAttribute('rel', 'noopener noreferrer');
     });
 });
