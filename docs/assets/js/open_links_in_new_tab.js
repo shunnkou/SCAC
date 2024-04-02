e5a9735ac4bf4476168abbd0cb4700a1a4a8fa71
shunnkou <jacehuang8@proton.me>
2024-04-02 17:20:42 -0400

    Open link in new tab and update mkdocs emoji module

--- docs/assets/js/open_links_in_new_tab.js
+++ docs/assets/js/open_links_in_new_tab.js
@@ -1,9 +1,15 @@
+// Wait for the DOM content to be fully loaded before executing the script
 document.addEventListener('DOMContentLoaded', function() {
-    // Select all links within the body text
-    var bodyLinks = document.querySelectorAll('.markdown a');
+    // Select all anchor elements within paragraph elements inside an element with the class 'md-content__inner'
+    var paragraphLinks = document.querySelectorAll('.md-content__inner p a');
     
-    // Loop through each body link and add the target attribute
-    bodyLinks.forEach(function(link) {
+    // Iterate over each selected link element
+    paragraphLinks.forEach(function(link) {
+        // Set the 'target' attribute of the link to '_blank' to open it in a new tab
         link.setAttribute('target', '_blank');
+        // Add the 'rel' attribute with the values 'noopener noreferrer' for security
+        // 'noopener' ensures that the new tab doesn't have access to the 'window.opener' property
+        // 'noreferrer' ensures that no referrer information is passed to the linked page
+        link.setAttribute('rel', 'noopener noreferrer');
     });
 });
